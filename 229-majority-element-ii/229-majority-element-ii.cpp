cd47618dbbfc5af5adbc9a59a70b516847449a89
Bhupesh Kumar Manuja <64948467+bhupesh21@users.noreply.github.com>
2022-07-11 21:36:35 +0530

    Time: 19 ms (62.56%), Space: 15.9 MB (11.94%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-    
-   vector<int>v;
-      int count=0;
-         map<int,int>m;
-        int n = nums.size();
-         for(int i=0;i<nums.size();i++)
-         {
-            m[nums[i]]++;//for count
+        vector<int>v;
+    map<int,int>m;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
         }
-        
-        for(auto it:m)
-        {
-            if(it.second >n/3)
+        for(auto it:m){
+            if(it.second>nums.size()/3){
                 v.push_back(it.first);
-         }
-        return v ;
-     }
-};
+            }
+        }
+        return v;
+    }
+    };
