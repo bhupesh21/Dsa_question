5b8298f923f44a6cded494b5b47f8ed63da6231f
Bhupesh Kumar Manuja <64948467+bhupesh21@users.noreply.github.com>
2022-07-14 12:43:17 +0530

    Time: 2970 ms (5.00%), Space: 51.1 MB (25.32%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,23 +1,46 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        if(nums.size()==0){
-            return 0;
+//         if(nums.size()==0){
+//             return 0;
+//         }
+//         sort(nums.begin(),nums.end());
+//    int ans=nums[0];
+//         int a=1;
+//         int curr=1;
+//         for(int i=1;i<nums.size();i++){
+//             if(nums[i]==ans+1){
+//                 curr++;
+//             }
+//             else if(nums[i]!=ans){
+//                 curr=1;
+//             }
+//             ans=nums[i];
+//             a=max(a,curr);
+//         }
+//         return a;
+//     }
+// };
+        
+        //using map
+        set<int>m;
+        for(int num:nums){
+            m.insert(num);
         }
-        sort(nums.begin(),nums.end());
-   int ans=nums[0];
-        int a=1;
-        int curr=1;
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]==ans+1){
-                curr++;
-            }
-            else if(nums[i]!=ans){
-                curr=1;
-            }
-            ans=nums[i];
-            a=max(a,curr);
+        int longstreak=0;
+        for(int num:nums){
+            if(!m.count(num-1)){
+                int current=num;
+                int currentstreak=1;
+             while(m.count(current+1)){
+                 current+=1;
+                 currentstreak+=1;
+             }
+                longstreak=max(longstreak,currentstreak);
+               
         }
-        return a;
-    }
-};
+               }
+               return longstreak;
+               }
+               
+               };
