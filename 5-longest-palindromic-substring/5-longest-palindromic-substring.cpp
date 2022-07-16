bb6caaaa29778ecd854bc725510bfecad12b483d
Bhupesh Kumar Manuja <64948467+bhupesh21@users.noreply.github.com>
2022-07-16 15:49:01 +0530

    Time: 14 ms (94.10%), Space: 7.2 MB (73.18%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -2,49 +2,42 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         
-        // edge cases
+        
         if(s.length() == 0 || s.length() == 1) return s;
         
-        // to hold max len and its starting index
-        int maxLenBeginIndex = 0;
+        
+        int start= 0;
         int maxLen = 1;
         
         int mid = 0;
         while(mid < s.length()){
-            
-            // calculating middle window
-            
-            int midBegin = mid;
+           int midBegin = mid;
             int midEnd = mid;
             
-            // handling even length palindromes; the middlemost chars will trivially match in even length case
-            // so expand the middle window as long as possible
-            while( midEnd + 1 < s.length() && s[midEnd] == s[midEnd + 1]){ midEnd++ ; }
             
-            // for next iteration
+            while( midEnd + 1 < s.length() && s[midEnd] == s[midEnd + 1]){ 
+                midEnd++ ; 
+            }    
             mid = midEnd + 1;
+          
+            int l =midBegin;
+            int r= midEnd;
             
-            // starting comparison in left and right windows
             
-            // in case of odd len palindrome; both start from mid;
-            // in case of even; midBegin and midEnd handles it all
-            int leftWindow = midBegin;
-            int rightWindow = midEnd;
-            
-            // expand the windows left and right simultaneously
-            while(leftWindow - 1 >= 0 && rightWindow + 1 < s.length() && s[leftWindow - 1] == s[rightWindow + 1]){
-                    leftWindow--;
-                    rightWindow++;
+            while(l- 1 >= 0 && r + 1 < s.length() && s[l- 1] == s[r + 1]){
+                    l--;
+                    r++;
             }
             
-            // update maxLen is currLen > maxLen
-            int currLen = rightWindow - leftWindow + 1;
+        
+            int currLen = r - l + 1;
             if( currLen  > maxLen ){
-                maxLenBeginIndex = leftWindow;
+                start = l;
                 maxLen = currLen;
             }
         }
         
-        return s.substr(maxLenBeginIndex, maxLen);
+        return s.substr(start, maxLen);
     }
 };
+    
