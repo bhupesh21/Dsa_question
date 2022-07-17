ab340cda9b9b5f4acc30db92b0588e38fb058b10
Bhupesh Kumar Manuja <64948467+bhupesh21@users.noreply.github.com>
2022-07-17 23:43:14 +0530

    Time: 8 ms (75.19%), Space: 6.6 MB (55.61%) - LeetHub

---
+++ 38-count-and-say/38-count-and-say.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        if(n==1)
+            return "1";
+        string res,tmp=countAndSay(n-1);
+        char c=tmp[0];
+        int count=1;
+        for(int i=1;i<tmp.size();i++)
+            if(tmp[i]==c)
+                count++;
+        else{
+            res+=to_string(count);
+            res.push_back(c);
+            c=tmp[i];
+            count=1;
+        }
+        res+=to_string(count);
+        res.push_back(c);
+        return res;
+    }
+};
+//         int count=0;
+//         int prev=n[0];
+//         for(int i=0;i<n.size();i++){
+            
+//         }
+        
+//     }
+// };
