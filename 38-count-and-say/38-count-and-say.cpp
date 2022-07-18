e4cd72ecb3e99dacbdc82b6bee78513c54bae7b1
Bhupesh Kumar Manuja <64948467+bhupesh21@users.noreply.github.com>
2022-07-18 11:16:06 +0530

    Time: 11 ms (61.86%), Space: 6.4 MB (89.33%) - LeetHub

--- 38-count-and-say/38-count-and-say.cpp
+++ 38-count-and-say/38-count-and-say.cpp
@@ -1,25 +1,52 @@
 class Solution {
 public:
     string countAndSay(int n) {
-        if(n==1)
+        if(n==1){
             return "1";
-        string res,tmp=countAndSay(n-1);
-        char c=tmp[0];
+        }
+        string s=countAndSay(n-1);
+        string res;
+        char c=s[0];
         int count=1;
-        for(int i=1;i<tmp.size();i++)
-            if(tmp[i]==c)
+    
+        for(int i=1;i<s.length();i++)
+            if(s[i]==c)
                 count++;
-        else{
+            
+            else{
+                res+=to_string(count);
+                res.push_back(c);
+                c=s[i];
+                count=1;
+            }
             res+=to_string(count);
             res.push_back(c);
-             c=tmp[i];
-             count=1;
-        }
-         res+=to_string(count);
-     res.push_back(c);
-        return res;
+            return res;
+        
+    
     }
 };
+     
+        
+    //     if(n==1)
+    //         return "1";
+    //     string res,s=countAndSay(n-1);
+    //     char c=tmp[0];
+    //     int count=1;
+    //     for(int i=1;i<tmp.size();i++)
+    //         if(tmp[i]==c)
+    //             count++;
+    //     else{
+    //         res+=to_string(count);
+    //         res.push_back(c);
+    //          c=tmp[i];
+    //          count=1;
+    //     }
+    //      res+=to_string(count);
+    //  res.push_back(c);
+    //     return res;
+    // }
+
 //         int count=0;
 //         int prev=n[0];
 //         for(int i=0;i<n.size();i++){
