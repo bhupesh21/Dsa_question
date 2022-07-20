ff8b0a000b3687ed8982819b2417dc3921ec01c6
Bhupesh Kumar Manuja <64948467+bhupesh21@users.noreply.github.com>
2022-07-20 22:44:21 +0530

    Time: 91 ms (5.11%), Space: 8.4 MB (95.26%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -1,44 +1,26 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        
         vector<int>v;
         for(int i=0;i<nums1.size();i++){
             for(int j=0;j<nums2.size();j++){
                 if(nums1[i]==nums2[j])
-                   // v.push_back(j);
+                   // v.push_back(nums[j]);
                 {
-                    int max_val=-1;
-                    for(int k=j;k<nums2.size();k++)
-                    {
-                        if(nums2[k]>nums2[j])
-                        {
-                            max_val=nums2[k];
-                            break;
-                        }   
-                    }
-                    v.push_back(max_val);
+        
+            int max=-1;
+            for(int k=j;k<nums2.size();k++){
+                if(nums2[k]>nums2[j]){
+                    max=nums2[k];
+                    break;
                 }
             }
+                v.push_back(max);
+            }
+            }
+         
         }
-        return v;}
-        
-    }; 
-                
-                
-                
-                
-                
-
-//                 if(nums1[1]<++nums2[j]){
-//                     ans=nums2[j];
-//                 }
-               
-//             }
-             
-//         }
-       
-//      return ans;
-//     }
+        return v;
+    }
     
-// };
+};
