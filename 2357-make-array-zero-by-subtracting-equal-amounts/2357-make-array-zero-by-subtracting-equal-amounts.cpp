a8bec9c243d7f2c809eeedf05abcc2713cef6445
Bhupesh Kumar Manuja <64948467+bhupesh21@users.noreply.github.com>
2022-08-03 14:00:51 +0530

    Time: 4 ms (59.39%), Space: 8.2 MB (78.44%) - LeetHub

---
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>0){
+                int store=nums[i];
+                count++;
+            
+            for(int j=i;j<nums.size();j++){
+                nums[j]=nums[j]-store;
+            }
+            }
+        }
+            return count;
+        
+    }
+};
