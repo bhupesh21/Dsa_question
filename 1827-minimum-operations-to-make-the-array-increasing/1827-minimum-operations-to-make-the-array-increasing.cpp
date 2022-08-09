3dd28b66fc828e1ebb7973d23f854f7282f525ae
Bhupesh Kumar Manuja <64948467+bhupesh21@users.noreply.github.com>
2022-08-09 13:35:22 +0530

    Time: 12 ms (95.94%), Space: 15.7 MB (97.95%) - LeetHub

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.cpp
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.cpp
@@ -1,14 +1,15 @@
-
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        int s=0;
+        int sum=0;
         for(int i=0;i<nums.size()-1;i++){
             if(nums[i+1]<=nums[i]){
-                s+=nums[i]-nums[i+1]+1;
+               sum+=nums[i]-nums[i+1]+1;
                 nums[i+1]+=nums[i]-nums[i+1]+1;
-            }    
+            }
         }
-        return s; 
+        return sum;
     }
 };
+
+
