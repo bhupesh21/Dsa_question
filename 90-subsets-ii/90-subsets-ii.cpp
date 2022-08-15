48739989d73d3a7b559211b7540ab30463f5e8f3
Bhupesh Kumar Manuja <64948467+bhupesh21@users.noreply.github.com>
2022-08-15 14:04:17 +0530

    Time: 3 ms (82.60%), Space: 7.6 MB (72.64%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>>ans;//to store  all subset'
+        vector<int>curr;//to store current inividual subset that we build
+        sort(nums.begin(),nums.end());//sort the arryay so that duplicate can adjacent
+        helper(nums,ans,curr,0);//we start from 0 index
+        return ans;
+        
+    }
+    void helper(vector<int>&nums,vector<vector<int>>& ans,vector<int>&curr,int idx){
+        ans.push_back(curr);//we incude current subset into final ans
+        for(int i=idx;i<nums.size();i++){//check for all possiblites
+if(i>idx && nums[i]==nums[i-1])
+    continue;//if duplicate we will continue
+            curr.push_back(nums[i]);
+            helper(nums,ans,curr,i+1);
+            curr.pop_back();//to get subset without nums[i]
+        }
+    }
+};
