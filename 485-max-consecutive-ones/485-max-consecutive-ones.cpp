be5c32fdd8b7c771120db66326bf5cf6decaf7b7
Bhupesh Kumar Manuja <64948467+bhupesh21@users.noreply.github.com>
2022-08-18 12:54:58 +0530

    Time: 43 ms (88.24%), Space: 36.1 MB (70.88%) - LeetHub

--- 485-max-consecutive-ones/485-max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/485-max-consecutive-ones.cpp
@@ -4,26 +4,19 @@ public:
         int count=0;
         int maxdiff=0;
         for(int i=0;i<nums.size();i++){
-            //if(nums[i]+nums[i+1])
-          //  maxdiff=nums[i]+nums[i+1];
+       
             if(nums[i]==0){
                 maxdiff=max(maxdiff,count);
                 count=0;
-            }
+            }//2&&3
             
             else
-                count++;
+                count++;//2&&3
         }
         maxdiff=max(maxdiff,count);
         return maxdiff;
                 
             
-            
-            
-        //     if(nums[i]==nums[i+1])
-        //         count++;
-        //     break;
-        // }
-        // return count;
+         
     }
 };
