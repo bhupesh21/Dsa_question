1465036857ee4ec714b9663e31cae91435e514c3
Bhupesh Kumar Manuja <64948467+bhupesh21@users.noreply.github.com>
2022-08-18 11:50:00 +0530

    Time: 297 ms (66.52%), Space: 78.4 MB (48.79%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     int minSetSize(vector<int>& arr) {
-        vector<int>v;
+        unordered_map<int,int>mp1;
         int ans=0;
         int n1=0;
-        unordered_map<int,int>mp1;
+        vector<int>v;
         for(int i=0;i<arr.size();i++){
-        mp1[arr[i]]++;
-        }
-        for(auto it:mp1){
-            v.push_back(it.second);
+            mp1[arr[i]]++;
             
+        }
+        for( auto it:mp1){
+            v.push_back(it.second);
         }
         if(v.size()==1)return 1;
         sort(v.begin(),v.end());
         for(int i=v.size()-1;i>=0;i--){
-            if(ans>=arr.size()/2){
-                return n1;
-            }
-            n1++;
-            ans+=v[i];
-            
+        if(ans>=arr.size()/2){
+            return n1;
         }
-        return 0;
-        
+         n1++;
+    ans=ans+v[i];
+
+    }
+   
+    return 0;
     }
 };
