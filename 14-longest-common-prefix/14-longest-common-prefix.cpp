8b557d2a0652d438d1675f1cfada3b9d3a6dd3f8
Bhupesh Kumar Manuja <64948467+bhupesh21@users.noreply.github.com>
2022-08-21 16:17:10 +0530

    Time: 4 ms (79.29%), Space: 9 MB (95.38%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -4,12 +4,11 @@ public:
         string sol="";
         for(int i=0;i<strs[0].size();i++){
             for(int j=1;j<strs.size();j++){
-                //forempty strs
-                if(i>strs[j].size() ||strs[0][i]!=strs[j][i])
-                    return sol;
+                if(i>strs[j].size()|| strs[0][i]!=strs[j][i])return sol;
             }
             sol+=strs[0][i];
         }
+        
         return sol;
     }
 };
