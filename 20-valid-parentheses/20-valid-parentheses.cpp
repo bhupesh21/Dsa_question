7d27ae80023db43d90f5e6f017e59a41973811ab
Bhupesh Kumar Manuja <64948467+bhupesh21@users.noreply.github.com>
2022-08-23 11:21:13 +0530

    Time: 5 ms (19.59%), Space: 6.5 MB (10.18%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,38 +1,22 @@
 class Solution {
 public:
     bool isValid(string s) {
-        char x;
-        stack<char>st;
+        stack<int>stk;
         for(int i=0;i<s.size();i++){
-            if(s[i]=='('||s[i]=='['||s[i]=='{'){
-                st.push(s[i]);
-                continue;
+            if(s[i]=='('||s[i]=='{'||s[i]=='['){
+                stk.push(s[i]);
             }
-            if(st.empty()){
-                return false;
+            else{
+              if(stk.size()==0)return false;
+                char ch=stk.top();
+                stk.pop();
+                if((s[i]==')' and  ch=='(' )or(s[i]==']' and ch=='[' )or(s[i]=='}' and ch=='{') )continue;
+                   else
+                   return false;
             }
-            switch(s[i]){
-                case ')':
-                    x=st.top();
-                    st.pop();
-                    if((x=='{'||x=='['))
-                        return false;
-                        break;
-                    case'}':
-                        x=st.top();
-                        st.pop();
-                        if((x=='('||x=='['))
-                            return false;
-                        break;
-            case']':
-         x=st.top();
-         st.pop();
-           if((x=='('||x=='{'))
-           return false;
-           break;
         }
-                                    }
-    
-    return (st.empty());
-                                    }
-                                    };
+       return stk.empty();
+       // return true;
+                  
+    }
+};
