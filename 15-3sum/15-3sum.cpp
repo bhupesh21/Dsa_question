4737e576e29e3cee0deed726dd7767704821abcb
Bhupesh Kumar Manuja <64948467+bhupesh21@users.noreply.github.com>
2022-08-24 23:22:59 +0530

    Time: 153 ms (49.83%), Space: 23.3 MB (35.50%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,56 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-         sort(nums.begin(),nums.end());
+        sort(nums.begin(),nums.end());
         vector<vector<int>>res;
-       
         for(int i=0;i<nums.size()-2;i++){
-            if(i==0 ||(i>0 && nums[i]!=nums[i-1])){
-                int lo=i+1,hi=nums.size()-1,sum=0-nums[i];
-                while(lo<hi){
-                    if(nums[lo]+nums[hi]==sum){
-                        vector<int>ans;
-                        ans.push_back(nums[i]);
-                        ans.push_back(nums[lo]);
-                        ans.push_back(nums[hi]);
-                        res.push_back(ans);
-                        
-                        while(lo<hi && nums[lo]==nums[lo+1])lo++;
-                        while(lo<hi && nums[hi]==nums[hi-1])hi--;
+            if(i==0 ||(i>0 &&nums[i]!=nums[i-1])){
+                int lo =i+1, high=nums.size()-1, sum=0-nums[i];
+                while(lo<high){
+                    if(nums[lo]+nums[high]==sum){
+                        vector<int>temp;
+                        temp.push_back(nums[i]);
+                        temp.push_back(nums[lo]);
+                        temp.push_back(nums[high]);
+                        res.push_back(temp);
+                        while(lo<high && nums[lo]==nums[lo+1])lo++;
+                        while(lo<high && nums[high]==nums[high-1])high--;
                         lo++;
-                        hi--;
+                        high--;
                     }
-                    else if(nums[lo]+nums[hi]<sum)lo++;
-                    else hi--;
-                    }
+                    else if(nums[lo]+nums[high]<sum)lo++;
+                    else high--;
                 }
             }
-        return res;
         }
+        return res;
+        
+    }
 };
-//          vector<vector<int>>v1;
-//         vector<int>v;
-//         sort(nums.begin(),nums.end());
-//         int n=nums.size();
-//         for(int i=0;i<n-1;i++){
-//             for(int j=i+1;j<n-1;j++){
-//                 for(int k=j+1;k<n-1;k++){
-//                     if(nums[i]+nums[j]+nums[k]==0){
-//                         v.push_back(nums[i]);
-//                         v.push_back(nums[j]);
-//                         v.push_back(nums[k]);
-                        
-//                         v1.push_back(v);
-//                         break;
-                    
-                        
-//                     }
-//                 }
-//             }
-//         }
-//         return v1;
-        
-//     }
-// };
-        
-        
