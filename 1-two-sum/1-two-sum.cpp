194ea64f9559c4d4d9a212939e9e963536035572
Bhupesh Kumar Manuja <64948467+bhupesh21@users.noreply.github.com>
2022-08-24 22:26:57 +0530

    Time: 19 ms (68.39%), Space: 11 MB (34.29%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int>v;
-      // sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-1;i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]+nums[j]==target){
-                    v.push_back(i);
-                    v.push_back(j);
-                }
-            }
-        }
-        return v;
+            vector<int>ans;
+        unordered_map<int,int>m;
+    
+        for(int i=0;i<nums.size();i++){
+            
+           if(m.find(target-nums[i])!=m.end()){
+               ans.push_back(m[target-nums[i]]);
+               ans.push_back(i);
+               return ans;
+           }
+             m[nums[i]]=i;
+      
+    }
+            return ans;
         
     }
 };
