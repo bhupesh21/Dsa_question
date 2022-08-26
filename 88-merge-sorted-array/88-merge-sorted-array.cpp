25dcdbd9c07ea34d63d9f9ea2e6f9cb3d9038b03
Bhupesh Kumar Manuja <64948467+bhupesh21@users.noreply.github.com>
2022-08-26 12:59:02 +0530

    Time: 6 ms (35.86%), Space: 9 MB (72.88%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,27 +1,3 @@
-// class Solution {
-// public:
-//     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-//    int arr3[n+m];
-//     int k = 0;
-//     for (int i = 0; i < n; i++) {
-//       arr3[k++] = nums1[i];
-//     }
-//     for (int i = 0; i < m; i++) {
-//       arr3[k++] = nums2[i];
-//     }
-//     sort(arr3,arr3+m+n);
-//     k = 0;
-//     for (int i = 0; i < n; i++) {
-//       nums1[i] = arr3[k++];
-//     }
-//     for (int i = 0; i < m; i++) {
-//       nums2[i] = arr3[k++];
-//     }
-
-//   }
-
-// };
-        
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
@@ -29,44 +5,8 @@ public:
         for(int i=m;i<nums1.size();i++){
             nums1[i]=nums2[j];
                 j++;
-            
         }
-            sort(nums1.begin(),nums1.end());
-                return;
-        }
-        
-    };
-//         int j=0;
-//         for(int i=m; i<nums1.size(); i++){
-//             nums1[i]=nums2[j];
-//             j++;
-//         }
-//         sort(nums1.begin(), nums1.end());
-//         return;
-//     }
-// };
-        
-        
-//      for(int i=0;i<nums1.size();i++){
-//          for(int j=0;j<nums2.size();j++){
-//              if(nums1[i]>nums2[j])
-//                  swap(nums1[i],nums2[j]);
-//              i++;
-//              j++;
-//              break;
-             
-//          }
-         
-         
-//      }
-
-//         for(int i=0;i<nums1.size();i++){
-//          for(int j=i+1;j<nums2.size();j++){
-//              if(nums1[i]>=nums2[j]){
-//                  swap(nums1[i],nums2[j]);
-//              }
-//          }
-//          }
-//         }
-    
-//     };
+        sort(nums1.begin(),nums1.end());
+        return;
+    }
+};
