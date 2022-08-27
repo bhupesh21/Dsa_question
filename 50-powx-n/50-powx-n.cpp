57c91a6999071c4a7560b71bf34a165f933fb631
Bhupesh Kumar Manuja <64948467+bhupesh21@users.noreply.github.com>
2022-08-28 00:32:56 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (96.17%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
   double myPow(double x, int n) {
- int ans;
+      int ans;
       if(n==0){
           return 1;
       }
-      if(n<0){
+      if(n<1){
           n=abs(n);
           x=1/x;
       }
@@ -13,7 +13,7 @@ public:
           return myPow(x*x,n/2);
       }
       else{
-          return x*myPow(x*x,n/2);
+         return x*myPow(x*x,n/2);
       }
   }
 };
