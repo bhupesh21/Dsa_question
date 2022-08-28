077890baf506cd78d73fe92ff0885a3b4c76ab26
Bhupesh Kumar Manuja <64948467+bhupesh21@users.noreply.github.com>
2022-08-28 15:40:03 +0530

    Time: 59 ms (5.72%), Space: 19.7 MB (13.83%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,51 +1,19 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map<int,int>m;
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            m[nums[i]]++;
+    
+    unordered_map<int,int>map;
+        for(int i=0;i<nums.size();i++){
+            map[nums[i]]++;
         }
-        for(auto it:m){
-            if(it.second>n/2)
-                return it.first;
+        for(auto x:map){
+            if(x.second>nums.size()/2){
+              return x.first;
+               
+            }
+           
         }
-        return -1;
+    
+         return -1;
     }
 };
-//         map<int,int>m;
-//         int n = nums.size();
-//         for(int i=0;i<nums.size();i++)
-//         {
-//             m[nums[i]]++;//for count
-//         }
-        
-//         for(auto it:m)
-//         {
-//             if(it.second >n/2)
-//                 return it.first;
-//         }
-//         return -1;
-//     }
-// };
-//         int n = nums.size();
-//         for(int i=0;i<n;i++)
-//         {
-//             int count=0;
-//             for(int j=0;j<n;j++)
-//             {
-//                 if(nums[i]==nums[j];
-//                 {
-//                     count++;
-//                 }
-//             }
-            
-//             if(count>n/2)
-//             {
-//                 return nums[i];
-//             }
-//         }
-        
-//         return -1;
-//     }
-// };
