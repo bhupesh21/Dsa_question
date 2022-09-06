b92f0f83cba8235eeb09e5ff1e5135321214e0b5
Bhupesh Kumar Manuja <64948467+bhupesh21@users.noreply.github.com>
2022-09-06 10:28:04 +0530

    Time: 40 ms (46.23%), Space: 19.2 MB (83.52%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -4,11 +4,9 @@ public:
         int count=0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]!=0){
-                swap(nums[i],nums[count]);
-            
-            count++;
+               swap(nums[i],nums[count]);
+                count++;
+            }
         }
-        
-    }
     }
 };
