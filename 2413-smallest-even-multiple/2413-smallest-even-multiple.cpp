8d1f10d2db83026a820da3ec9a97fbd015de8e01
Bhupesh Kumar Manuja <64948467+bhupesh21@users.noreply.github.com>
2022-09-18 15:59:54 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (50.00%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
-    int smallestEvenMultiple(int n) {
-        // if(n%2==0 &&n>2){
-        //     return n;
-        // }
-        if(n%2!=0 ){
-            return n*2;
-        }
-        return n;
-        
-        
-    }
+   int smallestEvenMultiple(int n) {
+    return n * (n % 2 + 1);
+}
 };
