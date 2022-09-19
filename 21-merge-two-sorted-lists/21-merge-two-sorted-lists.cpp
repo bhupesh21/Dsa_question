ca5e5500bee2d112ad262a0af37acb72c8718572
Bhupesh Kumar Manuja <64948467+bhupesh21@users.noreply.github.com>
2022-09-19 22:47:44 +0530

    Time: 11 ms (66.48%), Space: 14.8 MB (42.48%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -1,62 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* ptr1, *ptr2;
-        ptr1 = list1;
-        ptr2 = list2;
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1==NULL)return l2;
+        if(l2==NULL)return l1;
         
-        ListNode* head;
-        if(ptr1 && ptr2)
+        if(l1->val>l2->val) std::swap(l1,l2);
+        ListNode* res=l1;
+        while(l1!=NULL && l2!=NULL )
         {
-            if(ptr1->val <= ptr2->val)
-            {head = ptr1;
-            ptr1 = ptr1->next;}
-            else{
-                head = ptr2;
-                ptr2 = ptr2->next;
+            ListNode*temp=NULL;
+            
+            while(l1!=NULL && l1->val <=l2->val){
+                temp=l1;
+                l1=l1->next;
             }
+            temp->next=l2;
+            std::swap(l1,l2);
         }
-        else if(ptr1)
-        {
-            head = ptr1;
-            ptr1 = ptr1->next;
-        }
-        else if(ptr2)
-        {
-            head = ptr2;
-            ptr2 = ptr2->next;
-        }
-        else return NULL;
+        return res;
         
-        ListNode* ptr = head;
-        while(ptr1 && ptr2)
-        {
-            if(ptr1->val <= ptr2->val)
-            {
-                ptr->next = ptr1;
-                ptr1 = ptr1->next;
-            }
-            else{
-                ptr->next = ptr2;
-                ptr2 = ptr2->next;
-            }  
-            ptr = ptr->next;
-        }
-        
-        while(ptr1)
-        {
-            ptr->next = ptr1;
-            ptr1 = ptr1->next;
-            ptr = ptr->next;
-        }
-        
-        while(ptr2)
-        {
-            ptr->next = ptr2;
-            ptr2 = ptr2->next;
-            ptr = ptr->next;
-        }
-        
-        return head;
     }
 };
