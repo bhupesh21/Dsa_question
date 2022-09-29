cf550f299b9e0817a53f012bbbe05c8642676b81
Bhupesh Kumar Manuja <64948467+bhupesh21@users.noreply.github.com>
2022-09-29 11:29:00 +0530

    Time: 4 ms (42.93%), Space: 7.2 MB (21.94%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
@@ -6,21 +6,17 @@
  *     ListNode() : val(0), next(nullptr) {}
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };https://assets.leetcode.com/uploads/2021/07/23/lc-midlist2.jpg
+ * };
  */
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        int count=0;
-        ListNode*temp=head;
-        while(temp){
-            count++;
-            temp=temp->next;
+       ListNode*Slow=head;
+        ListNode*Fast=head;
+        while(Fast!=NULL && Fast->next!=NULL){
+            Slow=Slow->next;
+            Fast=Fast->next->next;
         }
-        temp=head;
-        for(int i=0;i<count/2;i++){
-            temp=temp->next;
-        }
-        return temp;
+        return Slow;
     }
 };
