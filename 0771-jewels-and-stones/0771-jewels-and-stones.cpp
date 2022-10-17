3e4c1b32047bf5a34d754a98de06ad3b0e9b4900
Bhupesh Kumar Manuja <64948467+bhupesh21@users.noreply.github.com>
2022-10-17 13:30:32 +0530

    Time: 11 ms (5.05%), Space: 6.6 MB (6.98%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.cpp
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
-    int numJewelsInStones(string jewels, string stones) {
-        int ans=0;
-       for(int i=0;i<jewels.size();i++){
-           for(int j=0;j<stones.size();j++){
-               if(jewels[i]==stones[j]){
-                   ans++;
-                   
-               }
-           }
-       }
-        return ans;
+    int numJewelsInStones(string J, string S) {
+
+        map<char, int> stoneCount;
+        for (auto stone : S) {
+            ++stoneCount[stone];
+        }
+
+        int res = 0;
+
+        for (auto jewel : J) {
+            res += stoneCount[jewel];
+        }
+
+        return res;
+
     }
 };
